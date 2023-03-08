e8d8022e5de0680c511b3c3baef4c4bac388c318
James Demlow <jdemlow@gmail.com>
2023-03-07 21:22:19 -0700

    add update functionality

--- src/App.js
+++ src/App.js
@@ -7,36 +7,53 @@ function App() {
   const [role, setRole] = useState("dev");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Caleb",
       role: "Developer",
       img: "https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
+      id: 2,
       name: "Sal",
       role: "Manager",
       img: "https://images.pexels.com/photos/4571943/pexels-photo-4571943.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
+      id: 3,
       name: "John",
       role: "Director of Eng.",
-      img: "https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      img: "https://images.pexels.com/photos/1844547/pexels-photo-1844547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
+      id: 4,
       name: "Melanie",
       role: "Software Engineer",
       img: "https://images.pexels.com/photos/4355346/pexels-photo-4355346.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
+      id: 5,
       name: "Corey",
       role: "The Devops guy",
       img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
+      id: 6,
       name: "Jake",
       role: "Senior Intern",
       img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
   ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -45,7 +62,6 @@ function App() {
           <input
             type="text"
             onChange={(e) => {
-              console.log(e.target.value);
               setRole(e.target.value);
             }}
           />{" "}
@@ -53,10 +69,12 @@ function App() {
             {employees.map((employee) => {
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
