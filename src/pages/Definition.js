9c0a6a45c8b47a16187d8ab7c257037a9084570e
James Demlow <jdemlow@gmail.com>
2023-03-20 17:56:19 -0600

    style the dictionary

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import DefinitionSearch from "../components/DefinitionSearch";
 import NotFound from "../components/NotFound";
 
 export default function Definition() {
@@ -70,6 +71,8 @@ export default function Definition() {
               </p>
             );
           })}
+          <p>Search again:</p>
+          <DefinitionSearch />
         </>
       ) : null}
     </>
