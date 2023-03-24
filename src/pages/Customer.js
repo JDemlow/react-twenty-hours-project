02e334de3778f18745db2a3718fcbf4fd25d93a6
James Demlow <jdemlow@gmail.com>
2023-03-23 20:22:35 -0600

    add shared.js file

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,9 +1,38 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import NotFound from "../components/NotFound";
 
 export default function Customer() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [customer, setCustomer] = useState();
+  const [notFound, setNotFound] = useState();
+  useEffect(() => {
+    const url = "http://localhost:8000/api/customers/" + id;
+    fetch(url)
+      .then((response) => {
+        if (response.status === 404) {
+          //render a 404 component in this page
+          setNotFound(true);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setCustomer(data.customer);
+      });
+  }, []);
   return (
     <>
-      <p>{id}</p>
+      {notFound ? <p>The customer with id {id} was not found.</p> : null}
+
+      {customer ? (
+        <div>
+          <p> {customer.id}</p>
+          <p> {customer.name}</p>
+          <p> {customer.industry}</p>
+        </div>
+      ) : null}
+      <Link to="customers">Go Back</Link>
     </>
   );
 }
